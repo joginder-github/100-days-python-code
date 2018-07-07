53712769989a999a412c853c8591ccce47b25473
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-08 00:40:36 +0530

    print_using_escape_sequences

---
+++ excersise/escape_sequences.py
@@ -0,0 +1,7 @@
+print("line a \\n line b")  # line a \n line b
+print("\\\"\\\'")           # \"\'
+print("this is \\\\ double backslash")  # this is \\ double backslash
+print("these are /\\/\\/\\/\\/\\ mountains")    # these are /\/\/\/\/\ mountains
+print("he is\tawesome") # he is   awesome
+print("\\\" \\n \\t \\\'")  # \" \n \t \'
+print(r"\" \n \t \'")   # \" \n \t \'
