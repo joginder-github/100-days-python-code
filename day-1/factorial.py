dc55e4396f84cd099f3c8436b1b3630f55102e84
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-10 02:25:41 +0530

    caculate_factorial_without_recursion

---
+++ day-1/factorial.py
@@ -0,0 +1,11 @@
+x=int(input("Enter the number of which factorial is to be calculated = "))
+fact=1
+for i in range(1,x+1):
+    fact=fact*i
+print("factorial of ",x," is ",fact)    
+
+
+# output:
+
+# Enter the number of which factorial is to be calculated = 5
+# factorial of  5  is  120
