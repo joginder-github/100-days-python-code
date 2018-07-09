7f63a8908f7a5ae7d88ae72e1cd1e52b5f908838
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-10 02:28:08 +0530

    concatenating_two_strings

---
+++ excersise/string_concatenation.py
@@ -0,0 +1,12 @@
+first=str(input("Enter first string = "))
+second=str(input("Enter the second string = "))
+third=first+" "+second
+print("concatenated string is ",third)
+
+
+
+# output:
+
+# Enter first string = joginder
+# Enter the second string = pal
+# concatenated string is  joginder pal
