b8561390d05441ab8c27e334f0d602041f876812
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-11 23:25:37 +0530

    pattern1

---
+++ excersise/pattern1.py
@@ -0,0 +1,19 @@
+for i in range(1,6):
+    for j in range(1,6):
+        if(j<=i):
+            print("*",end="\t")
+    print("\n")
+
+
+
+# output:
+
+# *
+
+# *       *
+
+# *       *       *
+
+# *       *       *       *
+
+# *       *       *       *       *
