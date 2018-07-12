440d25b538829395656b31621844565ea14605c4
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-12 20:32:39 +0530

    use_of_int()_fun

---
+++ excersise/int_fun.py
@@ -0,0 +1,13 @@
+num_one=int(input("Enter first number"))
+num_two=int(input("Enter second number"))
+total=num_one+num_two
+print("Total is",total)
+print("Total is "+str(total))
+
+
+# output:
+
+# Enter first number5
+# Enter second number3
+# Total is 8
+# Total is 8
