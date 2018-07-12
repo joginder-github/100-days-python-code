05e65a2b24c3f31530cc43e29947d8302b3aded4
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-12 20:33:04 +0530

    pattern_3

---
+++ excersise/pattern3.py
@@ -0,0 +1,18 @@
+for i in range(5,0,-1):
+    for j in range(1,6):
+        if(j<=i):
+            print(i,end="\t")
+    print("\n")
+
+
+# output:
+
+# 5       5       5       5       5
+
+# 4       4       4       4
+
+# 3       3       3
+
+# 2       2
+
+# 1
