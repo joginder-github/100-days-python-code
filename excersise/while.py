216d67d12eb6830e2a0ecad5b99c112640bdabc2
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-13 19:32:20 +0530

    while_loop_in_python

---
+++ excersise/while.py
@@ -0,0 +1,17 @@
+i=1
+while i<=10:
+    print("Hello world ",i)
+    i+=1
+
+# output:
+
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+# Hello world
