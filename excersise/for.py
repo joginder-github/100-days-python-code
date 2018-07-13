bbf9587f72c2df0906a9af892b13003ce51a8a62
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-13 19:31:05 +0530

    fo_loop

---
+++ excersise/for.py
@@ -0,0 +1,18 @@
+for i in range(10):
+    print("Hello World ",i)
+
+
+
+
+# output:
+
+# Hello World  0
+# Hello World  1
+# Hello World  2
+# Hello World  3
+# Hello World  4
+# Hello World  5
+# Hello World  6
+# Hello World  7
+# Hello World  8
+# Hello World  9
