15c3c02ccb33ea76eedd284c5622f14959959c0f
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-24 22:36:24 +0530

    check_a_year_is_leap_year_or_not

---
+++ excersise/leap_year.py
@@ -0,0 +1,17 @@
+year=int(input("Enter year="))
+if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("{0} is a leap year".format(year))
+       else:
+           print("{0} is not a leap year".format(year))
+   else:
+       print("{0} is a leap year".format(year))
+else:
+   print("{0} is not a leap year".format(year))
+
+
+# output:
+
+# Enter year=2015
+# 2015 is not a leap year
