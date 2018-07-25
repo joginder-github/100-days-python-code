c2d1e570ef6904873dbdbf7c56f3801de9b64c79
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-25 20:44:45 +0530

    to_find_the_area_of _a_triangle

---
+++ excersise/area_of_triangle.py
@@ -0,0 +1,17 @@
+a = float(input('Enter first side: '))
+b = float(input('Enter second side: '))
+c = float(input('Enter third side: '))
+
+# calculate the semi-perimeter
+s = (a + b + c) / 2
+
+# calculate the area
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %area)
+
+# output:
+
+# Enter first side: 9
+# Enter second side: 6
+# Enter third side: 8
+# The area of the triangle is 23.53
