ed685410f424bc3747797b673105f0e62ee774c2
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-25 22:00:46 +0530

    conversion_of_decimal_numbers_to_bin_octal_and_hexadecimal

---
+++ excersise/conversion.py
@@ -0,0 +1,14 @@
+dec = int(input("Enter any decimal number="))
+
+print("The decimal value of",dec,"is:")
+print(bin(dec),"in binary.")
+print(oct(dec),"in octal.")
+print(hex(dec),"in hexadecimal.")
+
+# output:
+
+# Enter any decimal number=26
+# The decimal value of 26 is:
+# 0b11010 in binary.
+# 0o32 in octal.
+# 0x1a in hexadecimal.
