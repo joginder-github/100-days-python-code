54ae4c1afd3cbc162dfb28b14b448dc9190f684a
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-27 23:36:06 +0530

    pattern14

--- excersise/pattern14.py
+++ excersise/pattern14.py
@@ -1,8 +1,27 @@
-for i in range(1,6):
+for i in range (1,5):
     k=1
+       
     for j in range(1,9):
-        if (j>=6-i and j<=4+i and k):
-            print("*",end="\t")
-        else:
-            print(" ",end="\t")
         
+        if j>=6-i and j<=4+i and k :
+            print("*",end=" ")
+            k=0
+            
+            
+        else:
+            print(" ",end=" ")
+            k=1
+
+
+    print("\n") 
+
+
+# output:
+
+#         *
+
+#       *   *
+
+#     *   *   *
+
+#   *   *   *   *
