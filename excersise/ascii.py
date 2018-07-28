b06e758dac9e913c17c0126099402f08e5206728
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-07-28 13:28:10 +0530

    print_ascii_value_of_any_character

---
+++ excersise/ascii.py
@@ -0,0 +1,11 @@
+# Program to find the ASCII value of the given character
+
+c = input("Enter a character: ")
+
+print("The ASCII value of '" + c + "' is",ord(c))
+
+
+# output:
+
+# Enter a character: b
+# The ASCII value of 'b' is 98
