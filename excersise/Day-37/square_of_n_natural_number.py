dbccbe6c072420eb2424c922fcda7de334c91d27
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-08-08 19:25:36 +0530

    print_series_of_square_of_n_natural_numbers

---
+++ excersise/Day-37/square_of_n_natural_number.py
@@ -0,0 +1,12 @@
+l=[]
+number=int(input("enter how many number="))
+for i in range(1,number+1):
+    data=i*i
+    l.append(data)
+print (l)
+
+
+# output:
+
+# enter how many number=10
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
