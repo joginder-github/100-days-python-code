01edece400361ffabf313d7df2cbd2ddb0076b88
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-08-11 21:04:53 +0530

    use_of_string_slicing

---
+++ excersise/Day-39/string_slicing.py
@@ -0,0 +1,29 @@
+#slicing / selecting sub sequences
+lang = "python"
+#syntax -[start argument : stop argument -1]
+print(lang[0:6])
+print(lang[0:5])
+print(lang[0:4])
+print(lang[0:3])
+print(lang[0:2])
+print(lang[0:1])
+print(lang[0:-1])
+print(lang[0:-2])
+print(lang[0:-3])
+print(lang[:])
+print(lang[1:])
+print(lang[:3])
+
+#output is 
+#python
+#pytho
+#pyth
+#pyt
+#py
+#p
+#pytho
+#pyth
+#pyt
+#python
+#ython
+#pyt
