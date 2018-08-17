5f67c984d0364aef25baefd7ffb6ac036db802e0
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-08-17 23:11:21 +0530

    shutdown_your_system

---
+++ excersise/Day-43/shutdown.py
@@ -0,0 +1,11 @@
+import os
+check = input("Want to shutdown your computer ? (y/n): ")
+if check == 'n':
+    exit()
+else:
+    os.system("shutdown /s /t 1")
+
+
+# output:
+
+# Want to shutdown your computer ? (y/n): n
