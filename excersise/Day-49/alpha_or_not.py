32b6fb44e0c031bf8be19fe54ac6c70e447f6d2b
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-08-25 22:37:43 +0530

    To check entered character is alphabet or not

---
+++ excersise/Day-49/alpha_or_not.py
@@ -0,0 +1,17 @@
+print("Enter '0' for exit.")
+ch = input("Enter any character: ")
+if ch == '0':
+    exit()
+else:
+    if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+    	print(ch, "is an alphabet.")
+    else:
+    	print(ch, "is not an alphabet.")
+
+
+
+# output:
+
+# Enter '0' for exit.
+# Enter any character: g
+# g is an alphabet.
