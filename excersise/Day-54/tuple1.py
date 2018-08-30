b915bde6a41921c560c0b8ce351a6566fc31d23d
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-08-30 23:22:16 +0530

    tuple1_example

---
+++ excersise/Day-54/tuple1.py
@@ -0,0 +1,19 @@
+# Python Tuple - Example Program
+
+tuple1 = ("python", "tuple", 1952, 2323, 432)
+
+print(tuple1)              # this will print the complete tuple
+print(tuple1[1:4])         # this will print the elements starting from 2nd till 4th
+print(tuple1[1:])          # this will print the elements starting from the 2nd element
+print(tuple1[0])           # this wil print the first element of the tuple
+print(tuple1 * 2)          # this will print the tuple two times
+
+
+# output:
+
+# ('python', 'tuple', 1952, 2323, 432)
+# ('tuple', 1952, 2323)
+# ('tuple', 1952, 2323, 432)
+# python
+# ('python', 'tuple', 1952, 2323, 432, 'python', 'tuple', 1952, 2323,
+# 432)
