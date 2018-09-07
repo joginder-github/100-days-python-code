0840f8f21c1ee5c10e026f4efe8cb06538372142
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-09-07 23:33:28 +0530

    program to list all files and folders in directory

---
+++ excersise/Day-61/list_files_in_directory.py
@@ -0,0 +1,13 @@
+import glob, os
+os.chdir("D:/")
+for file in glob.glob("*.*"):
+    print(file)
+
+
+# output:
+
+# Dev-Cpp 5.11 TDM-GCC 4.9.2 Setup.exe
+# IDM 6.28 Build 15 incl Patch [32bit + 64bit] + Crack Fake Serial Fixed [CrackingPatching]
+# Office_2016_16.0.4229.1002_x86-x64_v2.8.iso
+# PowerISO v6.6 Setup + Patch & Keygen
+# SRS(JOGINDER PAL).docx
