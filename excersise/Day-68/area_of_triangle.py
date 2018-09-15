06bbeb9f367fe6f95c20ef26ba27a9ec7da96103
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-09-15 23:34:56 +0530

    area of triangle

---
+++ excersise/Day-68/area_of_triangle.py
@@ -0,0 +1,19 @@
+class rectangle():
+    def __init__(self,breadth,length):
+        self.breadth=breadth
+        self.length=length
+    def area(self):
+        return self.breadth*self.length
+a=int(input("Enter length of rectangle: "))
+b=int(input("Enter breadth of rectangle: "))
+obj=rectangle(a,b)
+print("Area of rectangle:",obj.area())
+ 
+print()
+
+
+# output:
+
+# Enter length of rectangle: 5
+# Enter breadth of rectangle: 3
+# Area of rectangle: 15
