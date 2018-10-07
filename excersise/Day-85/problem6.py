e400a1a2636617875366817b3e0a198bfb34ab08
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-10-07 22:37:42 +0530

    problem6

---
+++ excersise/Day-85/problem6.py
@@ -0,0 +1,30 @@
+# Question
+# Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized.
+# Suppose the following input is supplied to the program:
+# Hello world
+# Practice makes perfect
+# Then, the output should be:
+# HELLO WORLD
+# PRACTICE MAKES PERFECT
+
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+# Solution:
+lines = []
+while True:
+    s = input("Enter any string:")
+    if s:
+        lines.append(s.upper())
+    else:
+        break
+
+for sentence in lines:
+    print(sentence)
+
+
+# output:
+
+# Enter any string:my name is joginder pal
+# Enter any string:
+# MY NAME IS JOGINDER PAL
