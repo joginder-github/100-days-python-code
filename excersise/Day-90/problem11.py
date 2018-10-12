66ddc25a7b4cd950bab9d68081ffc04adf709482
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-10-12 22:55:09 +0530

    problem11

---
+++ excersise/Day-90/problem11.py
@@ -0,0 +1,30 @@
+# Question:
+# Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+# Suppose the following input is supplied to the program:
+# Hello world!
+# Then, the output should be:
+# UPPER CASE 1
+# LOWER CASE 9
+
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+# Solution:
+s = input("Enter string using upper case and lower letter:")
+d={"UPPER CASE":0, "LOWER CASE":0}
+for c in s:
+    if c.isupper():
+        d["UPPER CASE"]+=1
+    elif c.islower():
+        d["LOWER CASE"]+=1
+    else:
+        pass
+print("UPPER CASE", d["UPPER CASE"])
+print("LOWER CASE", d["LOWER CASE"])
+
+
+# output:
+
+# Enter string using upper case and lower letter:MY NAME is joginder
+# UPPER CASE 6
+# LOWER CASE 10
