ab5e85379a06e16b429c66599b7c15f0f3beaef1
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-10-15 22:19:27 +0530

    problem14

---
+++ excersise/Day-93/problem14.py
@@ -0,0 +1,43 @@
+# Question:
+# Write a program that computes the net amount of a bank account based a transaction log from console input. The transaction log format is shown as following:
+# D 100
+# W 200
+
+# D means deposit while W means withdrawal.
+# Suppose the following input is supplied to the program:
+# D 300
+# D 300
+# W 200
+# D 100
+# Then, the output should be:
+# 500
+
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+# Solution:
+netAmount = 0
+while True:
+    s = input("Enter deposit amount withdrawal amount:")
+    if not s:
+        break
+    values = s.split(" ")
+    operation = values[0]
+    amount = int(values[1])
+    if operation=="D":
+        netAmount+=amount
+    elif operation=="W":
+        netAmount-=amount
+    else:
+        pass
+print(netAmount)
+
+
+# Output:
+
+# Enter deposit amount withdrawal amount:D 300
+# Enter deposit amount withdrawal amount:D 300
+# Enter deposit amount withdrawal amount:W 200
+# Enter deposit amount withdrawal amount:D 100
+# Enter deposit amount withdrawal amount:
+# 500
