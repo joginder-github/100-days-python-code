214022109f918ab533eb54d3a09e18537827f572
joginder-github <40789188+joginder-github@users.noreply.github.com>
2018-10-29 23:23:07 +0530

    problem99

---
+++ excersise/Day-99/problem20.py
@@ -0,0 +1,23 @@
+# Question:
+# Define a class named American which has a static method called printNationality.
+
+# Hints:
+
+# Use @staticmethod decorator to define class static method.
+
+# Solution
+class American(object):
+    @staticmethod
+    def printNationality():
+        print("America")
+
+anAmerican = American()
+anAmerican.printNationality()
+American.printNationality()
+
+
+# output:
+
+
+# America
+# America
